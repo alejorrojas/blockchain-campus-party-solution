bac7e075e1f3f73ab86d7413e63ba0132724ed74
Alejo <alejoivanrojas@gmail.com>
2022-09-18 20:25:57 -0300

    simplify

---
+++ contracts/data.js
@@ -0,0 +1,1 @@
+export const contractAddress = "0x0B2f7BBbb52a237aFd955A261760A20815aEf839"
--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,57 @@
 import { useState } from 'react'
 import { ethers } from "ethers"
 import contractABI from "../contracts/contract_abi.json"
-
-const contractAddress = "0x0B2f7BBbb52a237aFd955A261760A20815aEf839"
-
-
-function App() {
-  const [data, setData] = useState({
-    account: "No account available",
-    balance: "",
-    isConnected: false,
-    contractValue: "",
-    sendText: ""
-  })
+import { contractAddress } from '../contracts/data'
 
 const provider= new ethers.providers.Web3Provider(window.ethereum)
 const signer = provider.getSigner()
 const contract = new ethers.Contract(contractAddress, contractABI, signer)
 
+
+function App() {
+  const [account, setAccount] = useState("No account available")
+  const [contractValue, setContractValue] = useState("")
+
 const connectWallet = async()=>{
     if(window.ethereum){
-        console.log("yo got it")
-          window.ethereum.request({method: 'eth_requestAccounts'})
-          .then(response => (
-            setData({...data, 
-              isConnected: true,
-              account: response[0],            
-            })
-            ))
+        const respuesta = await window.ethereum.request({method: 'eth_requestAccounts'})
+        setAccount(respuesta[0])
     }
-    else console.log("please install metamask");
+    else window.open("https://metamask.io/download/", "_blank")
 }
 
 
-  const getCurrentValue = async()=>{
+  const readBlockchain = async()=>{
     const contractValue = await contract.get()
-    setData({...data, contractValue})
+    setContractValue(contractValue)
   }
 
-  const handleSubmit = (event) => {
-		event.preventDefault();
-		console.log('sending ' + event.target.value + ' to the contract');
-		contract.set(event.target.setText.value);
+  const writeBlockchain = (data) => {
+		data.preventDefault();
+		contract.set(data.target.userText.value);
 	}
 
   
   return (
     <div className="App">
       <div>
-          <img src="/token.png" className="logo" alt="Vite logo" />
-          <img src="/react.svg" className="logo react" alt="React logo" />
+          <img src="/token.png" className="logo"/>
+          <img src="/react.svg" className="logo react"/>
       </div>
+      
       <h1>Ethereum + React</h1>
+
       <div className="card">
-        <button onClick={()=>connectWallet()}>{data.isConnected ? "Disconnect wallet" : "Connect Wallet"}</button>
-        <p>{data.account}</p>
-        <p>{data.balance}</p>
+        <button onClick={()=>connectWallet()}>Connect Wallet</button>
+        <p>{account}</p>
       </div>
       <div className="card">
-        <form onSubmit={handleSubmit}>
-          <input id="setText" type="text"/>
+        <form onSubmit={writeBlockchain}>
+          <input id="userText" />
           <button type='submit'>SUBMIT</button>
         </form>
-        <button onClick={()=>getCurrentValue()}>Get current value</button>
-        <p>{data.contractValue}</p>
+        <button onClick={readBlockchain}>Get current value</button>
+        <p>{contractValue}</p>
       </div>
     </div>
   )
